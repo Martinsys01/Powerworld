01077046a1e5316ccdfda1d7c2fdfdb071b75889
Martinsys01 <msys@marsys.cz>
2024-12-07 02:19:56 +0100

    Delete Tepelka1 directory

--- Tepelka1/config/__init__.py
+++
@@ -1,4 +0,0 @@
-from .tuya_config import TuyaConfig
-from .pump_config import HeatPumpParameters
-
-__all__ = ['TuyaConfig', 'HeatPumpParameters']
--- Tepelka1/config/influxdb_config.py
+++
@@ -1,6 +0,0 @@
-INFLUXDB_CONFIG = {
-    'url': 'http://localhost:8086',
-    'token': 'KmMpoTxDrW1hJq-9xHEb4zIQhdw4SxyO1_isJvbgZ8FloRNHBPxcLEtOuR8GwwQ9pHQBx479mJ_2sUHhOHWmPw==',
-    'org': 'MARSYS',
-    'bucket': 'heat_pump'
-}
